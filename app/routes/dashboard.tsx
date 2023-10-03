543f8624bdb189b3fa09916f5002b42feb64b28d
nataliya-a <nkarmarkar@dons.usfca.edu>
2023-10-03 15:24:00 -0700

    feat: dashboard change to verify sign-up

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,3 +1,11 @@
 export default function Dashboard() {
-  return <>TODO</>;
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title font-bold text-2xl mb-4">Dashboard</h2>
+        </div>
+      </div>
+    </div>
+  );
 }
