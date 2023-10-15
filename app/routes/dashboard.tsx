81939d531f55fd46c1e99e29c86a004d6ac6b5d6
ashu <ashu@ashus-MacBook-Pro.local>
2023-10-15 00:58:46 -0700

    skeleton working

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,7 +1,7 @@
 import type { ActionFunction, LoaderFunction } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
 import { getUserSession } from "~/utils/userSession";
-import { useLoaderData } from "@remix-run/react";
+import { useLoaderData, useNavigate } from "@remix-run/react";
 import { destroySession, getSession } from "~/sessions.server";
 
 export const loader: LoaderFunction = async (args) => {
@@ -24,6 +24,12 @@ export const action: ActionFunction = async ({ request }) => {
 
 function DashboardPage() {
   const data = useLoaderData<any>();
+  const navigate = useNavigate();
+  function goToChatBox() {
+    let otherPerson = "60"; // this is where we set the id of the other person we want to chat with.
+
+    navigate(`/chatbox?otherPerson=${otherPerson}`);
+  }
 
   return (
     <div className="container h-screen flex justify-center items-center">
@@ -46,6 +52,25 @@ function DashboardPage() {
           Discover Influencers
         </button>
       </form>
+      <div>
+        <h1>Welcome to the Dashboard</h1>
+        {data && data.token ? (
+          <div>
+            <p>Token from the cookie: {data.token}</p>
+            {/* Your dashboard content for authenticated users */}
+          </div>
+        ) : (
+          <p>Please log in to access the dashboard.</p>
+        )}
+        <button
+          onClick={() => {
+            goToChatBox();
+          }}
+          className="btn btn-primary"
+        >
+          Chat with user 60
+        </button>
+      </div>
     </div>
   );
 }
