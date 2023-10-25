966cfeeab4aa8a7c64ff5468c8c2160a61902cee
nataliya-a <nkarmarkar@dons.usfca.edu>
2023-10-24 21:25:43 -0700

    feat: dashboard change to reflect usertype

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -3,6 +3,7 @@ import { json, redirect } from "@remix-run/node";
 import { getUserSession } from "~/utils/userSession";
 import { useLoaderData, useNavigate } from "@remix-run/react";
 import { destroySession, getSession } from "~/sessions.server";
+import { useEffect, useState } from "react";
 
 export const loader: LoaderFunction = async (args) => {
   const { userId, token } = await getUserSession(args);
@@ -14,6 +15,9 @@ export const loader: LoaderFunction = async (args) => {
 };
 
 export const action: ActionFunction = async ({ request }) => {
+  const data = useLoaderData<any>();
+  console.log(data.token);
+
   const session = await getSession(request.headers.get("Cookie"));
   return redirect("/log-in", {
     headers: {
@@ -24,12 +28,33 @@ export const action: ActionFunction = async ({ request }) => {
 
 function DashboardPage() {
   const data = useLoaderData<any>();
+
+  const [userType, setUserType] = useState(null);
   const navigate = useNavigate();
   function goToChatBox() {
     let otherPerson = "16"; // this is where we set the id of the other person we want to chat with.
 
     navigate(`/chatbox?otherPerson=${otherPerson}`);
   }
+
+  useEffect(() => {
+    // Make an HTTP GET request to fetch the user type
+    fetch("http://localhost:3000/user/getUser", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + String(data.token),
+      },
+    })
+      .then((response) => response.json())
+      .then((userData) => {
+        // Assuming you have a user type property in the response data
+        setUserType(userData.data.usertype);
+        console.log(userData.data.usertype);
+      })
+      .catch((error) => {
+        console.error("Error fetching user data", error);
+      });
+  }, [data.token]);
 
   return (
     <div className="container h-screen flex justify-center items-center">
@@ -44,15 +69,18 @@ function DashboardPage() {
         </button>
         <br />
         <br />
-        <button
-          className="btn btn-primary"
-          name="operation"
-          value="DISCOVER-INFLUENCERS"
-        >
-          Discover Influencers
-        </button>
+        {userType === "BRAND" && (
+          <button
+            className="btn btn-primary"
+            name="operation"
+            value="DISCOVER-INFLUENCERS"
+          >
+            Discover Influencers
+          </button>
+        )}
       </form>
       <div>
+        <br />
         <h1>Welcome to the Dashboard</h1>
         {data && data.token ? (
           <div>
@@ -62,14 +90,16 @@ function DashboardPage() {
         ) : (
           <p>Please log in to access the dashboard.</p>
         )}
-        <button
-          onClick={() => {
-            goToChatBox();
-          }}
-          className="btn btn-primary"
-        >
-          Chat with user 16
-        </button>
+        {userType === "BRAND" && (
+          <button
+            onClick={() => {
+              goToChatBox();
+            }}
+            className="btn btn-primary"
+          >
+            Chat with user 16
+          </button>
+        )}
       </div>
     </div>
   );
